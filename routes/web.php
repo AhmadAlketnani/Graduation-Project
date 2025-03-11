8b0869b5729478cf94e3bee98e0c79ae1a07882d
EzzatThary <ezzatthary198@gmail.com>
2025-03-11 14:27:13 +0200

    complete a categorise section task

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\Dashboard\StoreController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard\CategoryController;
 
 
 Route::resource('stores', StoreController::class);
+
+Route::resource('categories', CategoryController::class);
 
 Route::get('/', function ()  {
 return view('dashboard.layout.app');
@@ -13,3 +16,4 @@ return view('dashboard.layout.app');
 Route::get('admin', function () {
     return view('dashboard.layout.app');
 })->name('admin');
+// Route::resource('categories',)
