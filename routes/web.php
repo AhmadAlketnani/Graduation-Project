86ca222834222ed6088b401366308a54596ae319
EzzatThary <ezzatthary198@gmail.com>
2025-03-12 20:19:40 +0200

    create permissions table

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Dashboard\RoleController;
 use App\Http\Controllers\Dashboard\StoreController;
 use App\Http\Controllers\Dashboard\ProductController;
 use App\Http\Controllers\Dashboard\CategoryController;
+use App\Http\Controllers\Dashboard\PermissionController;
 
 
 Route::resource('stores', StoreController::class);
@@ -12,6 +13,8 @@ Route::resource('stores', StoreController::class);
 Route::resource('categories', CategoryController::class);
 Route::resource('Products ', ProductController::class);
 Route::resource('roles', RoleController::class);
+Route::resource('permissions ', PermissionController::class);
+
 
 Route::get('/', function ()  {
 return view('dashboard.layout.app');
