9d4aed03d660365f80c97e4c324da8d57b61dbcb
EzzatThary <ezzatthary198@gmail.com>
2025-03-16 12:26:06 +0200

    edit plane index

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <nav aria-label="breadcrumb" class="p-3 mb-3 rounded-1 d-flex justify-content-between "
+    {{-- <nav aria-label="breadcrumb" class="p-3 mb-3 rounded-1 d-flex justify-content-between "
         style="background: #d3d3d3 !important;">
         <h2 style="margin: 0 !important;">Categories</h2>
         <ol class="breadcrumb d-flex align-items-center" style="margin: 0 !important;">
@@ -10,7 +10,7 @@
             <li class="breadcrumb-item active">Categories</li>
 
         </ol>
-    </nav>
+    </nav> --}}
 
     <div class="tile mb-4">
         <div class="row">
