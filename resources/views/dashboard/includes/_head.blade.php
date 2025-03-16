429b551fe8f3f7d3d56c46cb4dffed9451952521
AhmadAlketnani <ahmadalktnany383@gmail.com>
2025-03-16 10:41:09 +0200

    edit asset files

--- resources/views/dashboard/includes/_head.blade.php
+++ resources/views/dashboard/includes/_head.blade.php
@@ -1,8 +1,7 @@
 <head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>Injaz Dashboard</title>
 
@@ -16,13 +15,19 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet" />
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
 
     <link rel="stylesheet" href="{{ asset('dashboard-asset/vendor/fonts/iconify-icons.css') }}" />
 
     <!-- Core CSS -->
     <!-- build:css assets/vendor/css/theme.css  -->
+
+
+  <link
+  rel="stylesheet"
+  href="{{ asset('dashboard-asset//vendor/libs/pickr/pickr-themes.css') }}"
+/>
 
     <link rel="stylesheet" href="{{ asset('dashboard-asset/vendor/css/core.css') }}" />
     <link rel="stylesheet" href="{{ asset('dashboard-asset/css/demo.css') }}" />
@@ -33,55 +38,31 @@
 
     <!-- endbuild -->
 
+    <link rel="stylesheet" href="{{ asset('dashboard-asset/vendor/fonts/flag-icons.css') }}" />
     <link rel="stylesheet" href="{{ asset('dashboard-asset/vendor/libs/apex-charts/apex-charts.css') }}" />
 
     <!-- Page CSS -->
 
     <!-- Helpers -->
     <script src="{{ asset('dashboard-asset/vendor/js/helpers.js') }}"></script>
+    <style type="text/css">
+        .layout-menu-fixed .layout-navbar-full .layout-menu,
+        .layout-menu-fixed-offcanvas .layout-navbar-full .layout-menu {
+          top: 64px !important;
+        }
+        .layout-page {
+          padding-top: 64px !important;
+        }
+        .content-wrapper {
+          padding-bottom: 54px !important;
+        }
+      </style>
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+        <script src="{{ asset('dashboard-asset/vendor/js/template-customizer.js') }}"></script>
+        <link rel="stylesheet" href="{{ asset('dashboard-asset/vendor/css/template-customizer.css') }}">
 
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
 
     <script src="{{ asset('dashboard-asset/js/config.js') }}"></script>
-  </head>
-
-
-
-{{-- <head>
-
-    <title>{{ env('APP_NAME') }}</title>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-
-    <!-- Main CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('dashboard-asset/css/main.css') }}">
-    <!-- Font-icon css-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
-    Noty
-    <link href="{{ asset('dashboard-asset/plugins/noty/noty.css') }}" rel="stylesheet">
-    <link href="{{ asset('dashboard-asset/plugins/noty/mint.css') }}" rel="stylesheet">
-    <script src="{{ asset('dashboard-asset/plugins/noty/noty.min.js') }}" type="text/javascript"></script>
-    Select2
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-
-    <style>
-        .btn-primary {
-            color: white !important;
-        }
-
-        label {
-            font-weight: bold;
-        }
-        .colorD3{
-            background: #d3d3d3 !important;
-        }
-    </style>
-</head> --}}
-
-
-
+    <style id="custom-css"></style>
+</head>
