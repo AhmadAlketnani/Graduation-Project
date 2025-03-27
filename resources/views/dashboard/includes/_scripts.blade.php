e886a13bdd28d69cfc95418fdc1b0cc82b1718ad
AhmadAlketnani <ahmadalktnany383@gmail.com>
2025-03-27 14:19:55 +0200

    edit assets

--- resources/views/dashboard/includes/_scripts.blade.php
+++ resources/views/dashboard/includes/_scripts.blade.php
@@ -4,17 +4,8 @@
 
 <script src="{{ asset('dashboard-asset/vendor/libs/popper/popper.js') }}"></script>
 <script src="{{ asset('dashboard-asset/vendor/js/bootstrap.js') }}"></script>
-<script src="{{ asset('dashboard-asset/vendor/libs/@algolia/autocomplete-js.js') }}"></script>
 
-
-
-<script src="{{ asset('dashboard-asset/vendor/libs/pickr/pickr.js') }}"></script>
 <script src="{{ asset('dashboard-asset/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-
-
-<script src="{{ asset('dashboard-asset/vendor/libs/hammer/hammer.js') }}"></script>
-
-<script src="{{ asset('dashboard-asset/vendor/libs/i18n/i18n.js') }}"></script>
 
 <script src="{{ asset('dashboard-asset/vendor/js/menu.js') }}"></script>
 
@@ -34,11 +25,6 @@
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 {{-- sweetalert2 --}}
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-
-{{-- <script src="{{ asset('dashboard-asset/js/jquery-3.7.0.min.js') }}"></script>
-<script src="{{ asset('dashboard-asset/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('dashboard-asset/js/main.js') }}"></script>
 select2
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> --}}
 
