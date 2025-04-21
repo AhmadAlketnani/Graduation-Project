42ab77e7d5d5702c437216686f6604bfaa13063e
Ahmad Alketnani <91073674+AhmadAlketnani@users.noreply.github.com>
2025-04-21 12:46:15 +0300

    Update _scripts.blade.php

--- resources/views/dashboard/includes/_scripts.blade.php
+++ resources/views/dashboard/includes/_scripts.blade.php
@@ -12,19 +12,6 @@
 <script src="{{asset('dashboard-assets/js/typeahead.js')}}"></script>
 <script src="{{asset('dashboard-assets/js/main.js')}}"></script>
 <script src="{{asset('dashboard-assets/vendor/js/custom/theme-switcher.js')}}"></script>
-=======
-<script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
-<script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
-<script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
-<script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-<script src="{{ asset('assets/vendor/libs/hammer/hammer.js') }}"></script>
-<script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
-<!-- endbuild -->
-<script src="{{ asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
-<link rel="stylesheet" href="{{ asset('assets/vendor/libs/toastr/toastr.js') }}" />
-<!-- Main JS -->
-<script src="{{ asset('assets/js/typeahead.js') }}"></script>
-<script src="{{ asset('assets/js/main.js') }}"></script>
 
 <script>
     $.ajaxSetup({
